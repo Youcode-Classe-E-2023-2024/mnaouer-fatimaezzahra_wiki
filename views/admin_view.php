e4b05b130a8b87daed59411f49d88b0f99c42537
fatima ezzahra mnaouer <fatimaezzahra.mnaouer@gmail.com>
2024-01-12 17:22:08 +0100

    add admin

--- views/admin_view.php
+++ views/admin_view.php
@@ -170,24 +170,30 @@
         <div class="container-fluid pt-4 px-4">
             <div class="bg-light text-center rounded p-4">
                 <div class="d-flex align-items-center justify-content-between mb-4">
-                    <h6 class="mb-0">List Product</h6>
-                    <a href="">Add Product</a>
+                    <h6 class="mb-0">List Categories</h6>
+                    <a href="">Add Category</a>
                 </div>
                 <div class="table-responsive">
                     <table class="table text-start align-middle table-bordered table-hover mb-0" id="productTable">
                         <thead>
                         <tr class="text-dark">
                             <!--                            <th scope="col"><input class="form-check-input" type="checkbox"></th>-->
-                            <th scope="col">IdProduct</th>
-                            <th scope="col">Title</th>
-                            <th scope="col">Description</th>
-                            <th scope="col">UserId</th>
-                            <th scope="col">Action</th>
+                            <th scope="col">IdCategory</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Create At</th>
+                            <th scope="col">Edit At</th>
                         </tr>
                         </thead>
                         <tbody>
-                        <tr>
 
+                        <tr>
+                            <td><input class="form-check-input" type="checkbox"></td>
+                            <td>01 Jan 2045</td>
+                            <td>INV-0123</td>
+                            <td>Jhon Doe</td>
+                            <td>$123</td>
+                            <td>Paid</td>
+                            <td><a class="btn btn-sm btn-primary" href="">Detail</a></td>
                         </tr>
 
                         </tbody>
