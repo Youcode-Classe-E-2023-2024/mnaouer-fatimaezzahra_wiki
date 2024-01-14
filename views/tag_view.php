dd014ee96ad9f4b575117bcc2b46ae74ec1eb6ed
fatima ezzahra mnaouer <fatimaezzahra.mnaouer@gmail.com>
2024-01-14 20:38:26 +0100

    update dashboard title

--- views/tag_view.php
+++ views/tag_view.php
@@ -49,8 +49,8 @@
     <!-- Sidebar Start -->
     <div class="sidebar pe-4 pb-3">
         <nav class="navbar bg-light navbar-light">
-            <a href="index.html" class="navbar-brand mx-4 mb-3">
-                <h3 class="text-primary"><i class="fa fa-hashtag me-2"></i>WIKI_ADMIN</h3>
+            <a href="index.php?page=admin" class="navbar-brand mx-4 mb-3">
+                <h3 class="text-primary"><i class="fa fa-hashtag me-2"></i>ADMIN</h3>
             </a>
             <div class="navbar-nav w-100">
                 <a href="index.php?page=admin" class="nav-item nav-link active"><i
