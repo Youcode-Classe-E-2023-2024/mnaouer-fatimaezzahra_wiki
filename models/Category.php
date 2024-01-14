0c3c9f6701df1a3d0b87975460be5be8c5114758
fatima ezzahra mnaouer <fatimaezzahra.mnaouer@gmail.com>
2024-01-14 18:13:11 +0100

    update category class

--- models/Category.php
+++ models/Category.php
@@ -2,5 +2,12 @@
 
 class Category
 {
+    static function getAll()
+    {
+        global $db;
+        $result = $db->query("SELECT * FROM categories 
+         ORDER BY create_at DESC, edit_at;");
 
+        return $result->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
